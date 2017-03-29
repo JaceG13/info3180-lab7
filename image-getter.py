687aac265ea39233732293fb3f1a86cc2b42313b
jaceg13 <jaygooden@hotmail.com>
2017-03-29 15:12:30 +0000

    initial commit

--- image-getter.py
+++ image-getter.py
@@ -6,21 +6,29 @@ url = "https://www.walmart.com/ip/54649026"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 
+images = []
+
+print images
+
+print "\n\nAND NOW.....\n\n"
+
 # This will look for a meta tag with the og:image property
 og_image = (soup.find('meta', property='og:image') or
                     soup.find('meta', attrs={'name': 'og:image'}))
 if og_image and og_image['content']:
-    print og_image['content']
-    print ''
+    images.append(og_image['content'])
+    
 
 # This will look for a link tag with a rel attribute set to 'image_src'
 thumbnail_spec = soup.find('link', rel='image_src')
 if thumbnail_spec and thumbnail_spec['href']:
-    print thumbnail_spec['href']
-    print ''
+    images.append(thumbnail_spec['href'])
+    
 
 
 image = """<img src="%s"><br />"""
 for img in soup.findAll("img", src=True):
-   print image % urlparse.urljoin(url, img["src"])
-   print ''
+   images.append(image % urlparse.urljoin(url, img["src"]))
+   
+
+print images
